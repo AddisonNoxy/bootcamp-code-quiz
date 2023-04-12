811a786d92de75c84e9bd49f9d89a42f54803a00
AddisonNoxy <drolknil@gmail.com>
2023-04-12 14:24:13 -0400

    added minimal styling and baseline javascript

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # bootcamp-code-quiz
+
+## Description
+
+## Installation
+N/A
+
+## Usage
+
+## Credits
+N/A
+
+## License
+Please refer to the LICENSE in the repo.
---
+++ script.js
@@ -0,0 +1,30 @@
+//add timer interval
+//add function for local storage of scores
+//add array list of questions and answers
+//add section for possible clickable answers
+//add section for correct or incorrect results
+var startButton = document.querySelector("#start-button");
+var currentQuestion = 0;
+
+var questions = ["What does the DOM stand for?",
+                "What does the setInterval method do?",
+                "How many items can fit in an array?",
+                "Which of the following methods can be used to query a specific element?"];
+
+console.log(document.body);
+
+var timerLength = 40;
+
+function startGame() {
+
+}
+
+function createQuestion() {
+
+}
+
+function showHighScores() {
+
+}
+
+startButton.addEventListener("click", startGame);
